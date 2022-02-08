244bf7c596ee6051309d7d883645485eb4660693
Giedrius Pakalniskis <giedrius.pakalniskis@gmail.com>
2022-02-08 22:37:35 +0200

    Change package name and bundle identifier

--- android/app/src/main/java/com/gpakalniskis/orientuokis/MainActivity.java
+++ android/app/src/main/java/com/gpakalniskis/orientuokis/MainActivity.java
@@ -1,4 +1,4 @@
-package com.orenteering;
+package com.gpakalniskis.orientuokis;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "orenteering";
+    return "orienteering";
   }
 }
