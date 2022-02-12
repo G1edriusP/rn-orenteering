a3c8516f0d57a839ef7bfa1e358fcd60c6289885
Giedrius Pakalniskis <giedrius.pakalniskis@gmail.com>
2022-02-12 16:08:14 +0200

    Firebase email auth

--- src/containers/Auth/LoginScreen.tsx
+++ src/containers/Auth/LoginScreen.tsx
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 // Components
-import { Button } from "react-native";
+import { Alert, Button } from "react-native";
+import auth, { FirebaseAuthTypes } from "@react-native-firebase/auth";
 
 // Types
 import { LoginScreenProps } from "constants/navigation/types";
@@ -12,11 +13,34 @@ import { Stacks } from "constants/navigation/routes";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 const LoginScreen = ({ navigation }: LoginScreenProps) => {
-  const onLoginPress = () => navigation.navigate(Stacks.HOME);
+  const [isLoading, setIsLoading] = useState<Boolean>(false);
+
+  const onLoginPress = (): void => {
+    auth()
+      .signInWithEmailAndPassword("giedrius.pakalniskis@nfq.lt", "testas123")
+      .then(() => setIsLoading(true)) // User has been successfully created
+      .catch((error: FirebaseAuthTypes.NativeFirebaseAuthError) => {
+        console.log(error.code);
+      });
+  };
+
+  const onAuthStateChanged = (user: FirebaseAuthTypes.User | null): void => {
+    if (user) {
+      console.log(user);
+      // navigation.navigate(Stacks.HOME, { user });
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
+    return subscriber;
+  }, []);
 
   return (
     <SafeAreaView>
       <Text>Login screen</Text>
+      <Text>{isLoading ? "true" : false}</Text>
       <Button title={"Login"} onPress={onLoginPress} />
     </SafeAreaView>
   );
