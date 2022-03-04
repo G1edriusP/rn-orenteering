9dda223957e427c865485663f1df08a3bd99a3ce
Giedrius Pakalniskis <giedrius.pakalniskis@gmail.com>
2022-03-04 18:57:16 +0200

    Add custom fonts

---
+++ src/constants/fonts.ts
@@ -0,0 +1,5 @@
+export const fontLight = "Fredoka-Light";
+export const fontRegular = "Fredoka-Regular";
+export const fontMedium = "Fredoka-Medium";
+export const fontSemiBold = "Fredoka-SemiBold";
+export const fontBold = "Fredoka-Bold";
